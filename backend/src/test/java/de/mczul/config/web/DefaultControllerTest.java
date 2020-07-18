31a8f587ffd2459c8d62f92db42e4a4f8430bf18
Christoph Zuleger <christoph.zuleger@gmail.com>
2020-07-18 22:41:09 +0200

    Refactorings

--- backend/src/test/java/de/mczul/config/web/DefaultControllerTest.java
+++ backend/src/test/java/de/mczul/config/web/DefaultControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.core.env.Environment;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -24,7 +23,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -62,13 +60,9 @@ class DefaultControllerTest {
         );
     }
 
-    @Autowired
-    Environment env;
-
     @ParameterizedTest
     @MethodSource("buildGetQueryArgs")
     void testGet(int pageIndex, int pageSize, List<ScheduledConfigEntry> expectedEntries) throws Exception {
-        LOG.info(Arrays.deepToString(env.getActiveProfiles()));
         List<ScheduledConfigDto> expectedDtos = scheduledConfigMapper.fromDomainList(expectedEntries);
         when(scheduledConfigRepository.findAll(any(Pageable.class))).thenReturn(new PageImpl<>(expectedEntries));
 
