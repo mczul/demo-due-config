d67186452d67b6000d2c8c6db9961937b1ccb743
Christoph Zuleger <christoph.zuleger@gmail.com>
2020-08-13 21:33:12 +0200

    Externalize cron configuration for cleanup task; Convert incoming keys to lowercase before saving;

--- backend/src/main/java/de/mczul/config/common/ScheduledConfigService.java
+++ backend/src/main/java/de/mczul/config/common/ScheduledConfigService.java
@@ -19,6 +19,8 @@ public class ScheduledConfigService {
 
     @Transactional
     public ScheduledConfigEntry set(ScheduledConfigEntry entry) {
+        // Assure uniform key representation
+        entry.setKey(entry.getKey().toLowerCase());
         return entryRepository.save(entry);
     }
 
@@ -28,7 +30,7 @@ public class ScheduledConfigService {
     }
 
     @Transactional
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(cron = "${de.mczul.config.cleanup.cron}")
     void cleanup() {
         List<ScheduledConfigEntry> obsolete = entryRepository.findOutdated();
         LOG.info("Found {} entries that are obsolete", obsolete.size());
